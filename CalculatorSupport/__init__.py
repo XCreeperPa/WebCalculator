7c771539aed133e33f5d0c93742b93d2727ff28a
Noob_0 <117826101+Noob-0-GitHub@users.noreply.github.com>
2023-09-29 20:53:25 +0800

    CalculatorSupport v1.0

---
+++ CalculatorSupport/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+# |=========================|
+# |      Coded by Noob_0    |
+# | Noob_0-main@outlook.com |
+# |=========================|
+__author__ = "Noob_0"
+__version__ = "1.0"
+__license__ = "MIT"
+
+from .CalculatorSupport import *
