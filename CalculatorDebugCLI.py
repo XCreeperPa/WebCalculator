ff5dd65bbde5f0675749a5dd98d66c2f3cd5cf20
XCreeperPa <136904010+XCreeperPa@users.noreply.github.com>
2023-10-05 02:09:01 +0800

    Web前端 用户手册页面更新

--- CalculatorDebugCLI.py
+++ CalculatorDebugCLI.py
@@ -1,7 +1,9 @@
-from CalculatorSupport.CalculatorSupport import calc_main
+from CalculatorSupport import Fraction
+from CalculatorSupport.CalculatorSupport import calc_main, set_DefaultCalcType
 from traceback import print_exc
 
 if __name__ == '__main__':
+    set_DefaultCalcType(Fraction)
     while 1:
         try:
             calc_main(input(">>> "))
